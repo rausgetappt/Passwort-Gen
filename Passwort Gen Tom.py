24749b384a8610f8bb7bbe9a6f0243b1b1683e80
Leon <60196035+rausgetappt@users.noreply.github.com>
2023-02-24 08:46:03 +0100

    Update Passwort Gen Tom.py

--- Passwort Gen Tom.py
+++ Passwort Gen Tom.py
@@ -1,3 +1,9 @@
+# Anwendung:    Passwort Generator
+# Autor:        Tom Hente
+# Datum:        21.02.2023
+# Version:      1.0
+
+
 #Einbindung Bibiliothek Random
 import random
 
@@ -12,9 +18,12 @@ while len(werte) < 10:                          #Laufe bis 10 Objekte in Liste W
     if password > 32:                           #Wenn Passwort größer als 32 ist füge es Liste hinzu
         werte.append(password)
 
-for ziffern in werte:                           #Schleife 
-    value = int(ziffern)
-    output_list.append(value)
+for ziffern in werte:                           #Nehme alle Ziffern aus Werte
+    value = int(ziffern)                        #nehme neue Variable Value und ändere Ziffern in int
+    output_list.append(value)                   #Füge Value der Liste Output hinzu
+
+
+#Gebe alle Objekte (von 0-9) aus der Liste Output aus, ohne Leerzeichen dazwischen (sep='')
 
 print(chr(output_list[1]),chr(output_list[2]),chr(output_list[3]),chr(output_list[4]),chr(output_list[5]),
 chr(output_list[6]),chr(output_list[7]),chr(output_list[8]),chr(output_list[9]),chr(output_list[0]),sep='')
